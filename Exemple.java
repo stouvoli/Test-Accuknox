d9a82053924a97f3b268c8adf5f52c6047f738f4
King Ballo <stouvoli@gmail.com>
2025-05-21 20:06:57 +0000

    Update Exemple.java

--- Exemple.java
+++ Exemple.java
@@ -3,6 +3,7 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 import javax.servlet.http.HttpServletRequest;
 
+// Commentaires
 public class UserDAO {
 
     public void unsafeQuery(HttpServletRequest request) throws Exception {
